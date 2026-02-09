631c71b7533ee538ccdf3e07974a2d0712629011
Ole <pawlowski.ole@icloud.com>
2026-02-09 17:45:35 +0100

    Initial Commit

---
+++ MyMed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMed
+//
+//  Created by Ole Pawlowski on 09.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMed/MyMedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMedApp.swift
+//  MyMed
+//
+//  Created by Ole Pawlowski on 09.02.26.
+//
+
+import SwiftUI
+
+@main
+struct MyMedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
